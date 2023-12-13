87406264bf10e43fa3b4107ddecfd7c2259138fa
Josué Salazaku <josuesalazaku@gmail.com>
2023-12-13 09:21:31 +0100

    Start a server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-const http = require("http");
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send({ code: 200, msg: "OK" });
+});
+
+app.listen(3000, () => {
+  console.log("Running on htthttp://127.0.0.1:3000");
+});
