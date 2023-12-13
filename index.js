0251517c502af5e4e035344f5d03c0a128a4ebf7
Josué Salazaku <josuesalazaku@gmail.com>
2023-12-13 09:53:36 +0100

    Send first respons

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send({ code: 200, msg: "OK" });
 });
 
+app.get("/profile", (req, res) => {
+  res.status(403).send();
+});
+
 app.listen(3000, () => {
-  console.log("Running on htthttp://127.0.0.1:3000");
+  console.log("Running on http://127.0.0.1:3000");
 });
